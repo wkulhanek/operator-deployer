dff13a4740bf57983425017df6f029a3cae661b4
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-01-22 14:31:27 +0000

    Added Jenkins Operator deploy script

---
+++ deploy_jenkins_operator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ansible-playbook ./smooth-operator.yaml \
+  -e "operator_state=present" \
+  -e "operator_name=jenkins" \
+  -e "operator_plural_name=jenkins" \
+  -e "operator_short_name=je" \
+  -e "operator_group=gpte.opentlc.com" \
+  -e "operator_version=v1alpha1" \
+  -e "operator_kind=clusterwide" \
+  -e "operator_namespace=gpte-operators" \
+  -e "operator_image=quay.io/wkulhanek/jenkins-operator:v0.0.2"
